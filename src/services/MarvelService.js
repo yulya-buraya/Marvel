42b2154f262a62cfd122127f69e7ef5743c5b155
buraya.yuliya <burayayulya374@gmail.com>
2023-10-02 20:06:31 +0300

    implemented MarverService and RandomChar

---
+++ src/services/MarvelService.js
@@ -0,0 +1,31 @@
+class MarvelService{
+    _apiBase='https://gateway.marvel.com:443/v1/public/';
+    _apiKey='apikey=3d431a5cab15312a95a156a2189c4e3f';
+    getResource = async(url)=>{
+    let res = await fetch(url);
+
+    if(!res.ok){
+        throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    }
+    return await res.json();
+   } 
+   getAllCharapters =async()=>{
+    const res =  this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    return res.data.results.map(this._transformCharacter);
+   }
+   getCharapter =async(id)=>{
+    const res = await this.getResource(`${this._apiBase}/characters/${id}?${this._apiKey}`);
+    return this._transformCharacter(res.data.results[0]);
+   }
+   _transformCharacter = (char) =>{
+     return {
+        name: char.name,
+        description: char.description? `${char.description.slice(0,210)}...`:"This is no description for this charapter",
+        thumbnail: char.thumbnail.path + "." + char.thumbnail.extension,
+        homepage: char.urls[0].url,
+        wiki: char.urls[1].url,
+     }
+   }
+}
+
+export default MarvelService;
