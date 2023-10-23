935153c932a80288eed31649a172f6a8bbf9b4eb
buraya.yuliya <burayayulya374@gmail.com>
2023-10-23 16:19:54 +0300

    update react version

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/app/App';
-import './style/style.scss';
-import MarvelService from './services/MarvelService';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/app/App";
+import "./style/style.scss";
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
-
